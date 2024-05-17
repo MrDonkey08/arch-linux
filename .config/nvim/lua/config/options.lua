e155cc23ae7d00dc29e3f849f8363ed063dc2c52
Alan <113253976+MrDonkey08@users.noreply.github.com>
2024-05-17 16:35:21 -0600

    Nvim

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,22 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- General Settings
+
+vim.o.number = true
+vim.o.relativenumber = false
+vim.o.wrap = true
+vim.o.linebreak = true
+vim.o.breakindent = true
+vim.o.mouse = "a"
+vim.o.encoding = "UTF-8"
+
+-- Identation
+
+vim.o.expandtab = false -- Tab Identation
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.autoindent = true
+vim.o.smarttab = true
+vim.o.softtabstop = 4
