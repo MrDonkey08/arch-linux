5748ce02ee75264b54aee9fac830bfb6bd453d16
Alan <113253976+MrDonkey08@users.noreply.github.com>
2024-06-11 00:34:01 -0600

    Symlinks Update

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,16 +1,19 @@
 # !/bin/bash
 
 ln -s ~/Drives/github/new-cursos ~/Desktop/new-cursos
-ln -s ~/Drives/github/cursos ~/Desktop/cursos
+ln -s ~/Drives/github/blog ~/Desktop/blog
+ln -s ~/Drives/github/blog/_posts ~/Desktop/blog/_posts
 
 ln -s ~/Drives/github/escuela ~/Desktop/escuela
 ln -s ~/Drives/github/escuela/4to ~/Desktop/4to
 ln -s ~/Drives/github/escuela/5to ~/Desktop/5to
+ln -s ~/Drives/github/escuela/6to ~/Desktop/6to
 
 ln -s ~/Drives/google/Recursos/ ~/Desktop/recursos
 ln -s ~/Drives/google/Classroom/ ~/Desktop/classroom
 ln -s ~/Drives/google/Classroom/4to ~/Desktop/drive-4to
 ln -s ~/Drives/google/Classroom/5to ~/Desktop/drive-5to
+ln -s ~/Drives/google/Classroom/6to ~/Desktop/drive-6to
 
 sudo su
 
