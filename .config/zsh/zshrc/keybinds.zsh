15a39c17dd5d39c55555962319cac2d7b66f089b
Alan <113253976+MrDonkey08@users.noreply.github.com>
2024-06-21 23:42:36 -0600

    Fixed Kb and Font

--- .config/zsh/zshrc/keybinds.zsh
+++ .config/zsh/zshrc/keybinds.zsh
@@ -1,8 +1,9 @@
+# Use emacs keybindings even if our EDITOR is set to vi
+bindkey -e
+
 bindkey "^[[H" beginning-of-line
 bindkey "^[[F" end-of-line
 bindkey "^[[3~" delete-char
 bindkey "^[[1;3C" forward-word
 bindkey "^[[1;3D" backward-word
 
-# Use emacs keybindings even if our EDITOR is set to vi
-bindkey -e
